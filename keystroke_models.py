bc79d215fa674d8ba08175cdca2a63951f7e4c1f
shin <873705@gmail.com>
2024-08-12 13:33:10 +0900

    Ignore fleet files; Fix state loading
    Add the `.fleet/` directory to `.gitignore` to prevent fleet-related files from being tracked. Ensure the `load_latest_state` function in `keystroke_simulator_app.py` returns immediately if the state is not found, preventing potential errors.

--- keystroke_models.py
+++ keystroke_models.py
@@ -6,45 +6,45 @@ from PIL import Image
 
 @dataclass
 class UserSettings:
-    start_stop_key: str = "`"
-    key_pressed_time_min: int = 95
-    key_pressed_time_max: int = 135
-    delay_between_loop_min: int = 100
-    delay_between_loop_max: int = 150
-    start_sound: str = "start.mp3"
-    stop_sound: str = "stop.mp3"
+	start_stop_key: str = "`"
+	key_pressed_time_min: int = 95
+	key_pressed_time_max: int = 135
+	delay_between_loop_min: int = 100
+	delay_between_loop_max: int = 150
+	start_sound: str = "start.mp3"
+	stop_sound: str = "stop.mp3"
 
 
 @dataclass
 class EventModel:
-    event_name: Optional[str] = None
-    latest_position: Optional[tuple] = None
-    clicked_position: Optional[tuple] = None
-    latest_screenshot: Optional[Image.Image] = None
-    held_screenshot: Optional[Image.Image] = None
-    ref_pixel_value: Optional[tuple] = None
-    key_to_enter: Optional[str] = None
+	event_name: Optional[str] = None
+	latest_position: Optional[tuple] = None
+	clicked_position: Optional[tuple] = None
+	latest_screenshot: Optional[Image.Image] = None
+	held_screenshot: Optional[Image.Image] = None
+	ref_pixel_value: Optional[tuple] = None
+	key_to_enter: Optional[str] = None
 
-    def __iter__(self):
-        for field in fields(self):
-            yield field, getattr(self, field.name)
+	def __iter__(self):
+		for field in fields(self):
+			yield field, getattr(self, field.name)
 
-    def __str__(self):
-        field_strings = [f"{field.name}={value}" for field, value in self]
-        return f"EventModel({', '.join(field_strings)})"
+	def __str__(self):
+		field_strings = [f"{field.name}={value}" for field, value in self]
+		return f"EventModel({', '.join(field_strings)})"
 
 
 @dataclass
 class ProfileModel:
-    name: Optional[str] = None
-    event_list: Optional[list[EventModel]] = None
+	name: Optional[str] = None
+	event_list: Optional[list[EventModel]] = None
 
-    def __iter__(self):
-        for event in self.event_list or []:
-            yield event
+	def __iter__(self):
+		for event in self.event_list or []:
+			yield event
 
-    def __str__(self):
-        event_strings = [str(event) for event in self]
-        return (
-            f"ProfileModel(name='{self.name}', event_list=[{', '.join(event_strings)}])"
-        )
+	def __str__(self):
+		event_strings = [str(event) for event in self]
+		return (
+			f"ProfileModel(name='{self.name}', event_list=[{', '.join(event_strings)}])"
+		)
