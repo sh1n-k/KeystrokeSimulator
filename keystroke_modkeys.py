f91433a6a9f96b0f364514ead4a2ff8a28eab418
shin <873705@gmail.com>
2024-08-23 22:26:24 +0900

    fix(modKeys): 설정 저장, 불러오기

--- keystroke_modkeys.py
+++ keystroke_modkeys.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import pickle
 import os
+from typing import Dict, Any
 
 from loguru import logger
 
@@ -20,57 +21,49 @@ class ModificationKeysWindow(tk.Toplevel):
         self.grab_set()
         self.focus_force()
 
-        self.valid_keys = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        self.valid_keys = frozenset("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        self.labels = ("Alt", "Ctrl", "Shift")
+        self.rows = []
 
         self.create_widgets()
         self.load_profile()
         WindowUtils.center_window(self)
 
     def create_widgets(self):
-        self.labels = ["Alt", "Ctrl", "Shift"]
-        self.rows = []
-
-        for j, label in enumerate(self.labels):
-            row = []
-
-            lbl = ttk.Label(self, text=f"{label}")
-            lbl.grid(row=j, column=1, padx=5, pady=5)
-            row.append(lbl)
-
-            chk_var = tk.BooleanVar()
-            chk = ttk.Checkbutton(self, variable=chk_var)
-            chk.grid(row=j, column=2, padx=5, pady=5)
-            row.append(chk_var)
-
-            cmb_var = tk.StringVar(value="PressKey")
-            cmb = ttk.Combobox(
-                self, textvariable=cmb_var, values=["PressKey"], width=10
-            )
-            cmb.grid(row=j, column=3, padx=5, pady=5)
-            cmb.bind("<KeyPress>", lambda e, var=cmb_var: self.update_combobox(e, var))
-            row.append(cmb_var)
-            row.append(tk.StringVar(value="PressKey"))  # Previous value
-
-            pass_lbl = ttk.Label(self, text="Pass")
-            pass_lbl.grid(row=j, column=4, padx=5, pady=5)
-            row.append(pass_lbl)
-
-            pass_var = tk.BooleanVar(value=False)
-            pass_chk = ttk.Checkbutton(
-                self,
-                variable=pass_var,
-                command=lambda v=cmb_var, c=pass_var: self.toggle_pass(v, c),
-            )
-            pass_chk.grid(row=j, column=5, padx=5, pady=5)
-            row.append(pass_var)
-
-            self.rows.append(row)
+        for idx, label in enumerate(self.labels):
+            self.create_row(idx, label)
 
         save_button = ttk.Button(self, text="Save(Enter)", command=self.save)
         save_button.grid(row=len(self.labels), column=0, columnspan=6, pady=10)
 
         self.bind("<Return>", lambda e: self.save())
         self.bind("<Escape>", lambda e: self.destroy())
+
+    def create_row(self, idx: int, label: str):
+        ttk.Label(self, text=label).grid(row=idx, column=1, padx=5, pady=5)
+
+        chk_var = tk.BooleanVar()
+        ttk.Checkbutton(self, variable=chk_var).grid(row=idx, column=2, padx=5, pady=5)
+
+        cmb_var = tk.StringVar(value="PressKey")
+        cmb = ttk.Combobox(self, textvariable=cmb_var, values=["PressKey"], width=10)
+        cmb.grid(row=idx, column=3, padx=5, pady=5)
+        cmb.bind(
+            "<KeyPress>", lambda e, v=cmb_var, i=idx: self.update_combobox(e, v, i)
+        )
+
+        prev_value_var = tk.StringVar(value="PressKey")
+
+        ttk.Label(self, text="Pass").grid(row=idx, column=4, padx=5, pady=5)
+
+        pass_var = tk.BooleanVar(value=False)
+        ttk.Checkbutton(
+            self,
+            variable=pass_var,
+            command=lambda v=cmb_var, p=pass_var, i=idx: self.toggle_pass(v, p, i),
+        ).grid(row=idx, column=5, padx=5, pady=5)
+
+        self.rows.append((chk_var, cmb_var, prev_value_var, pass_var))
 
     def load_profile(self):
         profile_path = os.path.join(self.profiles_dir, f"{self.profile_name}.pkl")
@@ -93,64 +86,63 @@ class ModificationKeysWindow(tk.Toplevel):
             logger.info(
                 f"Profile '{self.profile_name}' does not exist. Starting with default values."
             )
+            self.destroy()
 
-    def display_saved_values(self, modification_keys):
-        for i, row in enumerate(self.rows):
-            key = self.labels[i].lower()
+    def display_saved_values(self, modification_keys: Dict[str, Any]):
+        for idx, key in enumerate(self.labels):
+            key = key.lower()
             if key in modification_keys:
-                row[1].set(modification_keys[key]["enabled"])  # Set checkbox
-                row[2].set(modification_keys[key]["value"])  # Set combobox value
-                row[3].set(modification_keys[key]["value"])  # Set previous value
-                row[5].set(modification_keys[key]["pass"])  # Set pass checkbox
+                row = self.rows[idx]
+                row[0].set(modification_keys[key]["enabled"])  # Set checkbox
+                row[1].set(modification_keys[key]["value"])  # Set combobox value
+                row[2].set(modification_keys[key]["value"])  # Set previous value
+                row[3].set(modification_keys[key]["pass"])  # Set pass checkbox
                 if modification_keys[key]["pass"]:
-                    self.toggle_pass(row[2], row[5])
+                    self.toggle_pass(row[1], row[3], idx)
 
-    def update_combobox(self, event, var):
-        value = event.char.upper()
-        if value in self.valid_keys:
-            logger.debug(f"value: {value}")
-            var.set(value)
-            return "break"
+    def update_combobox(self, event, var: tk.StringVar, idx: int):
+        if not self.rows[idx][3].get():  # Check if Pass checkbox is not checked
+            value = event.char.upper()
+            if value in self.valid_keys:
+                logger.debug(f"value: {value}")
+                var.set(value)
+                return "break"
 
-    def toggle_pass(self, cmb_var, pass_var):
-        index = self.rows.index(next(row for row in self.rows if row[2] == cmb_var))
-        prev_value_var = self.rows[index][3]  # Get the previous value variable
+    def toggle_pass(self, cmb_var: tk.StringVar, pass_var: tk.BooleanVar, idx: int):
+        prev_value_var = self.rows[idx][2]  # Get the previous value variable
 
         if pass_var.get():
             prev_value_var.set(cmb_var.get())  # Store the current value
             cmb_var.set("Pass")
-            for child in self.grid_slaves():
-                if isinstance(child, ttk.Combobox) and child.cget(
-                    "textvariable"
-                ) == str(cmb_var):
-                    child.config(state="readonly")
-                    break
+            self.update_combobox_state(cmb_var, "readonly")
         else:
-            for child in self.grid_slaves():
-                if isinstance(child, ttk.Combobox) and child.cget(
-                    "textvariable"
-                ) == str(cmb_var):
-                    child.config(state="normal")
-                    cmb_var.set(prev_value_var.get())  # Restore the previous value
-                    break
+            cmb_var.set(prev_value_var.get())  # Restore the previous value
+            self.update_combobox_state(cmb_var, "normal")
+
+    def update_combobox_state(self, cmb_var: tk.StringVar, state: str):
+        for child in self.grid_slaves():
+            if isinstance(child, ttk.Combobox) and child.cget("textvariable") == str(
+                cmb_var
+            ):
+                child.config(state=state)
+                break
 
     def save(self):
-        modification_keys = {}
-        for i, row in enumerate(self.rows):
-            key = self.labels[i].lower()
-            value = "Pass" if row[5].get() else row[2].get()
-            modification_keys[key] = {
-                "enabled": row[1].get(),
-                "value": value,
-                "pass": row[5].get(),
+        modification_keys = {
+            label.lower(): {
+                "enabled": row[0].get(),
+                "value": "Pass" if row[3].get() else row[1].get(),
+                "pass": row[3].get(),
             }
+            for label, row in zip(self.labels, self.rows)
+        }
 
         profile_path = os.path.join(self.profiles_dir, f"{self.profile_name}.pkl")
+        profile = ProfileModel(name=self.profile_name)
+
         if os.path.exists(profile_path):
             with open(profile_path, "rb") as f:
                 profile = pickle.load(f)
-        else:
-            profile = ProfileModel(name=self.profile_name)
 
         profile.modification_keys = modification_keys
 
